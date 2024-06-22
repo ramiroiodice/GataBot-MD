4f3c0403acab3b72639ee18f6fc2d39d47537648
GL YT MX <148560941+glytglobal@users.noreply.github.com>
2024-06-22 10:30:23 -0300

    Update owner-add_del_owner.js

--- plugins/owner-add_del_owner.js
+++ plugins/owner-add_del_owner.js
@@ -2,26 +2,24 @@
 const handler = async (m, { conn, text, args, usedPrefix, command }) => {
   const datas = global
   const idioma = datas.db.data.users[m.sender].language
-  const _translate = archive.parse(fs.readFileSync(`./lib/idiomas/total-idiomas.js`))
-  const tradutor = _translate.plugins.owner_add_del_owner
-
-  const why = `${tradutor.texto1[0]} ${usedPrefix + command} @${m.sender.split('@')[0]}*\n*◉ ${usedPrefix + command} ${m.sender.split('@')[0]}*\n*◉ ${usedPrefix + command} ${tradutor.texto1[0]}`;
+  
+  const why = `${usedPrefix + command} @${m.sender.split('@')[0]}*\n*◉ ${usedPrefix + command} ${m.sender.split('@')[0]}*\n*◉ ${usedPrefix + command} `;
   const who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : false;
   if (!who) return conn.reply(m.chat, why, m, {mentions: [m.sender]});
   switch (command) {
     case 'addowner':
       const nuevoNumero = who;
       global.owner.push([nuevoNumero]);
-      await conn.reply(m.chat, tradutor.texto2, m);
+      await conn.reply(m.chat, m, fake);
       break;
     case 'delowner':
       const numeroAEliminar = who;
       const index = global.owner.findIndex(owner => owner[0] === numeroAEliminar);
       if (index !== -1) {
         global.owner.splice(index, 1);
-        await conn.reply(m.chat, tradutor.texto3, m);
+        await conn.reply(m.chat, fake, m);
       } else {
-        await conn.reply(m.chat, tradutor.texto4, m);
+        await conn.reply(m.chat, fake, m);
       }
       break;
   }
