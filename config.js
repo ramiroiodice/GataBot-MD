a422d0cf7f695bbc0ad2c011ef600c0fac906751
ramiroiodice <144454307+ramiroiodice@users.noreply.github.com>
2024-06-21 22:01:22 -0300

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ import { en, es, id, ar, pt } from "./lib/idiomas/total-idiomas.js"
 // [EN] > Add the number that will be Bot and those that will be owners.
 global.owner = [
 ["5492355505090", '𝙊𝙬𝙣𝙚𝙧 💖🐈', true], //𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 💖🐈
-["5493795319022"], 
+["5493794297363"], 
 ["584245737254"], ["5492266613038"], ["5217294888993"], ["51948705559"]]
 
 global.mods = []
