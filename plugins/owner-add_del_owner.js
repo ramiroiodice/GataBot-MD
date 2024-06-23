936e84ef0256b863ba325795f3816dadc5f4ab75
ramiroiodice <144454307+ramiroiodice@users.noreply.github.com>
2024-06-22 22:06:14 -0300

    Update owner-add_del_owner.js

--- plugins/owner-add_del_owner.js
+++ plugins/owner-add_del_owner.js
@@ -3,7 +3,7 @@
   const idioma = datas.db.data.user[m.sender].lenguage
   
   const why = `PUEDE AGREGAR O ELIMINAR OWNERS DEL BOT DE ESTA FORMA:\n*${usedPrefix + command} @${m.sender.split('@')[0]}*\n*◉ ${usedPrefix + command} ${m.sender.split('@')[0]}*\n*◉ ${usedPrefix + command}*`;
-  const who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : true;
+  const who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : false;
   if (!who) return conn.reply(m.chat, why, m, {mentions: [m.sender]});
   switch (command) {
     case 'addowner':
@@ -13,7 +13,7 @@
       break;
     case 'delowner':
       const numeroAEliminar = who;
-      const index = global.owner.findIndex(owner => owner[0] === numeroAEliminar);
+      const index = global.owner.findIndex(owner => owner[1] === numeroAEliminar);
       if (index !== -1) {
         global.owner.splice(index, 1);
         await conn.reply(m.chat, m);
