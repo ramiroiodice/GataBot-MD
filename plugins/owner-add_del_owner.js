06a2f694bb08b967beb8aa648bf5c16f8efbb6dd
ramiroiodice <144454307+ramiroiodice@users.noreply.github.com>
2024-06-22 22:10:46 -0300

    Update owner-add_del_owner.js

--- plugins/owner-add_del_owner.js
+++ plugins/owner-add_del_owner.js
@@ -1,24 +1,26 @@
-  const handler = async (m, { conn, text, args, usedPrefix, command }) => {
+const handler = async (m, { conn, text, args, usedPrefix, command }) => {
   const datas = global
-  const idioma = datas.db.data.user[m.sender].lenguage
-  
-  const why = `PUEDE AGREGAR O ELIMINAR OWNERS DEL BOT DE ESTA FORMA:\n*${usedPrefix + command} @${m.sender.split('@')[0]}*\n*◉ ${usedPrefix + command} ${m.sender.split('@')[0]}*\n*◉ ${usedPrefix + command}*`;
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
+  const tradutor = _translate.plugins.owner_add_del_owner
+
+  const why = `${tradutor.texto1[0]} ${usedPrefix + command} @${m.sender.split('@')[0]}*\n*◉ ${usedPrefix + command} ${m.sender.split('@')[0]}*\n*◉ ${usedPrefix + command} ${tradutor.texto1[0]}`;
   const who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : false;
   if (!who) return conn.reply(m.chat, why, m, {mentions: [m.sender]});
   switch (command) {
     case 'addowner':
       const nuevoNumero = who;
       global.owner.push([nuevoNumero]);
-      await conn.reply(m.chat, m);
+      await conn.reply(m.chat, tradutor.texto2, m);
       break;
     case 'delowner':
       const numeroAEliminar = who;
-      const index = global.owner.findIndex(owner => owner[1] === numeroAEliminar);
+      const index = global.owner.findIndex(owner => owner[0] === numeroAEliminar);
       if (index !== -1) {
         global.owner.splice(index, 1);
-        await conn.reply(m.chat, m);
+        await conn.reply(m.chat, tradutor.texto3, m);
       } else {
-        await conn.reply(m.chat, m);
+        await conn.reply(m.chat, tradutor.texto4, m);
       }
       break;
   }
